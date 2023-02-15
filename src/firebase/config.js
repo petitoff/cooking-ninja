6eceb7e4d2af8d2aa964a90718d17f8fdff7eabc
Błażej Domagała <petitoff@outlook.com>
2023-02-15 14:57:55 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  // get config from .env
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
