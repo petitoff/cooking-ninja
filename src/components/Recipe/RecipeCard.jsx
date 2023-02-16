1a69bbbc8769d98376a23b2438853ce3eefad5f8
Błażej Domagała <petitoff@outlook.com>
2023-02-16 18:50:20 +0100

    Deleting Firestore Data

--- src/components/Recipe/RecipeCard.jsx
+++ src/components/Recipe/RecipeCard.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 import { useTheme } from "../../hooks/useTheme";
+import DeleteIcon from "../../assets/delete-icon.svg";
+
+import { projectFirestore } from "../../firebase/config";
 
 const RecipeCard = ({ recipe }) => {
   const { mode } = useTheme();
+
+  const handleDelete = () => {
+    projectFirestore.collection("recipes").doc(recipe.id).delete();
+  };
 
   return (
     <div className={`card ${mode}`}>
@@ -10,6 +17,12 @@ const RecipeCard = ({ recipe }) => {
       <p>{recipe.cookingTime} to make.</p>
       <div>{recipe.method.substring(0, 100)}...</div>
       <Link to={`/recipe/${recipe.id}`}>Cook This</Link>
+      <img
+        className="delete"
+        onClick={handleDelete}
+        src={DeleteIcon}
+        alt="delete"
+      />
     </div>
   );
 };
